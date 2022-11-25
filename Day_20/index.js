b1bc88068e674bc075016a55b105b0c33147af31
daudujohn <daudujoe@gmail.com>
2022-11-25 14:26:46 +0100

    Initial commit

--- Day_20/index.js
+++ Day_20/index.js
@@ -1,2 +1,13 @@
 const https = require('https');
 const fs = require('fs');
+const path = require('path')
+const options = {
+    key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
+};
+
+let server = https.createServer(options, (req, res) => {
+    res.end();
+})
+
+server.listen(5000);
